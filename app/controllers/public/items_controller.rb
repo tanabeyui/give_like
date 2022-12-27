ce0cdfb853bea2d38ea7d6c257374355c03ad63d
Yui Tanabe <ytogt356@gmail.com>
2022-12-27 15:06:03 +0000

    [Add] 会員側：楽天プレゼントランキングページ(items/index)の作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,17 @@
 class Public::ItemsController < ApplicationController
   def index
-  end
-
-  def search
+    @rankingSearchItems = RakutenWebService::Ichiba::Item.search(keyword: 'プレゼント', sort: '-reviewCount')
+    @items = []
+    @items.concat(@rankingSearchItems.page(1).to_a)
+    @items.concat(@rankingSearchItems.page(2).to_a)
+    @items.concat(@rankingSearchItems.page(3).to_a)
+    @items.concat(@rankingSearchItems.page(4).to_a)
+    @items.pop(20)
   end
 
   def show
+  end
+
+  def search
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-  namespace :public do
-    get 'items/index'
-    get 'items/search'
-    get 'items/show'
+  scope module: :public do
+    resources :items, only: [:index, :show]
   end
+
   devise_for :end_users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
