4dbd16a2638b82f85b8ea28c5e99cedb22ad81ac
Yui Tanabe <ytogt356@gmail.com>
2023-01-10 12:53:11 +0000

    [Update] エンドユーザーの年代表記の追加

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -10,4 +10,24 @@ class EndUser < ApplicationRecord
 
   enum gender: { male: 0, female: 1, not_selected: 2 }
 
+
+
+  def era
+    age = (Date.today.strftime("%Y%m%d").to_i - self.birth_day.strftime('%Y%m%d').to_i)
+    if age < 199999
+      p "10代"
+    else age = 200000 < 299999
+      p "20代"
+    elsif age = 300000 < 399999
+      p "30代"
+    elsif age = 400000 < 499999
+      p "40代"
+    elsif age = 500000 < 599999
+      p "50代"
+    elsif age = 600000 < 699999
+      p "60代"
+    else age > 700000
+      p "70代"
+    end
+  end
 end
