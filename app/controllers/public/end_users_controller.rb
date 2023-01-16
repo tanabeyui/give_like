3a460c8a3e3699f78b4f25404b3a3f3cb942872f
Yui Tanabe <ytogt356@gmail.com>
2023-01-16 14:40:48 +0000

    [Add] 会員情報編集ページを作成

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,16 +1,33 @@
 class Public::EndUsersController < ApplicationController
   def show
-    @end_user = current_end_user
+    @end_user = EndUser.find_by(screen_name: params[:screen_name])
     @favorites = @end_user.favorites.order(created_at: :desc).limit(5)
     @want_items = @end_user.want_items.order(created_at: :desc).limit(5)
     @reviews = @end_user.reviews.order(created_at: :desc).limit(5)
-    
+
     @categorys = Review.all.map(&:category).uniq
   end
 
   def edit
+    @end_user = current_end_user
+  end
+
+  def update
+    @end_user = current_end_user
+    if @end_user.update(end_user_params)
+      redirect_to end_user_path(@end_user.screen_name)
+    else
+      render :edit
+    end
   end
 
   def confirm
+  end
+
+
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:screen_name, :name, :email, :gender, :birth_day)
   end
 end
