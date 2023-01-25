9959be334baf3c4a3da7e197168292b0a18218ac
Yui Tanabe <ytogt356@gmail.com>
2023-01-25 17:46:49 +0000

    [Update] 会員側トップページにレビューランキング絞り込み機能を追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,19 @@
 class Public::HomesController < ApplicationController
+  before_action :set_search
+
   def top
     @responses = Response.group(:present_genre).order('count(id) desc').first(10)
     @items = RakutenWebService::Ichiba::Item.search(keyword: 'プレゼント', sort: '-reviewCount').first(5)
     @reviews = Review.group(:code).order("avg(evaluation) desc").first(5)
+  end
+
+
+
+  private
+
+  def set_search
+    @q = Review.ransack(params[:q])
+    @search_reviews = @q.result(distinct: true)
+    @ranking_searchs = @q.result(distinct: true).group(:code).order("avg(evaluation) desc")
   end
 end
