5b477fdc40840c6e86a461f56c05dce518291aaf
Yui Tanabe <ytogt356@gmail.com>
2023-01-25 18:44:56 +0000

    [Update] 楽天プレゼントランキングページの表示件数変更

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,9 +6,7 @@ class Public::ItemsController < ApplicationController
     @items = []
     @items.concat(@rankingSearchItems.page(1).to_a)
     @items.concat(@rankingSearchItems.page(2).to_a)
-    @items.concat(@rankingSearchItems.page(3).to_a)
-    @items.concat(@rankingSearchItems.page(4).to_a)
-    @items.pop(20)
+    @items.pop(10)
   end
 
   def show
