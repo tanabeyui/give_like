5309283e7ca7675ec96a7fa8e4005003e932bd18
Yui Tanabe <ytogt356@gmail.com>
2023-01-25 18:38:29 +0000

    [Update] 会員側トップページに男女別・年齢別アンケート結果を表示

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,6 +3,17 @@ class Public::HomesController < ApplicationController
 
   def top
     @responses = Response.group(:present_genre).order('count(id) desc').first(10)
+    @male_responses = Response.where(gender: "0").group(:present_genre).order('count(id) desc').first(3)
+    @female_responses = Response.where(gender: "1").group(:present_genre).order('count(id) desc').first(3)
+
+    @teens_responses = Response.where(age: "1").group(:present_genre).order('count(id) desc').first(1)
+    @twenties_responses = Response.where(age: "2").group(:present_genre).order('count(id) desc').first(1)
+    @thirties_responses = Response.where(age: "3").group(:present_genre).order('count(id) desc').first(1)
+    @forties_responses = Response.where(age: "4").group(:present_genre).order('count(id) desc').first(1)
+    @fifties_responses = Response.where(age: "5").group(:present_genre).order('count(id) desc').first(1)
+    @sixties_responses = Response.where(age: "6").group(:present_genre).order('count(id) desc').first(1)
+    @seventies_responses = Response.where(age: "7").group(:present_genre).order('count(id) desc').first(1)
+
     @items = RakutenWebService::Ichiba::Item.search(keyword: 'プレゼント', sort: '-reviewCount').first(5)
     @reviews = Review.group(:code).order("avg(evaluation) desc").first(5)
   end
