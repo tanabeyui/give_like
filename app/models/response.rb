bd36130611dc8bba2188acaf09ad5d43f20fe1a9
Yui Tanabe <ytogt356@gmail.com>
2023-02-05 01:53:35 +0000

    [Update] 会員側アンケート投票ページの作成

--- app/models/response.rb
+++ app/models/response.rb
@@ -6,7 +6,7 @@ class Response < ApplicationRecord
                                       boss: 7, subordinate: 8, coworker: 9, business_people: 10, acquaintance: 11, other_people: 12 }
   enum gifted_event: { forgot_date: 0, birthday: 1, christmas: 2, father_day: 3, mother_day: 4, anniversary: 5, valentine: 6, white_day: 7,
                            birthday_20th: 8, celebrate_job: 9, wedding_gift: 10, baby_gift: 11, housewarming: 12, birthday_60th: 13, other_day: 14 }
-  enum present_genre: { flower: 0, letter: 1, jewelry: 2, cosmetic: 3, branded_goods: 4, fashion: 5, large_appliance: 6, small_appliance: 7, tablet: 8,
+  enum present_genre: { flower: 0, letter: 1, jewelry: 2, cosmetic: 3, branded_goods: 4, fashion: 5, large_appliance: 6, small_appliance: 7, cash: 8,
                            foods: 9, sweets: 10, alcohol: 11, interior: 12, necessities: 13, hobby: 14, baby_supplies: 15, pet_supplies: 16,
                                                                                     vehicle: 17, house: 18, catalog_gift: 19, travel: 20, other_present: 21 }
 
