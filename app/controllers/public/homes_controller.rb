bd6b06a5b92826df91f3eb45076b1941cc23d36c
Yui Tanabe <ytogt356@gmail.com>
2023-02-09 15:29:22 +0000

    [Fix] 会員側トップページのレビューランキング集計を公開レビューに制限

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -8,7 +8,7 @@ class Public::HomesController < ApplicationController
     @male_responses = Response.where(gender: "0").group(:present_genre).order('count(id) desc').first(3)
     @female_responses = Response.where(gender: "1").group(:present_genre).order('count(id) desc').first(3)
     @items = RakutenWebService::Ichiba::Item.search(keyword: 'プレゼント', sort: '-reviewCount').first(5)
-    @reviews = Review.group(:code).order("avg(evaluation) desc").first(5)
+    @reviews = Review.disclosed.group(:code).order("avg(evaluation) desc").first(5)
   end
 
 
