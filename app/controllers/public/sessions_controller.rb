44e06d164546b3517b8875034c9973a1a3e2c929
Yui Tanabe <ytogt356@gmail.com>
2023-02-12 17:27:34 +0000

    [Update] 退会済みユーザーのログイン制限を追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :end_user_state, only: [:create]
 
   def after_sign_in_path_for(resource)
     end_user_path(current_end_user.screen_name)
@@ -7,6 +7,20 @@ class Public::SessionsController < Devise::SessionsController
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+
+
+
+  protected
+
+  def end_user_state
+    @end_user = EndUser.find_by(screen_name: params[:end_user][:screen_name])
+    return if !@end_user
+
+    if @end_user.valid_password?(params[:end_user][:password]) && @end_user.is_deleted == "unsubscribe"
+      flash[:danger] = "無効なIDです。新規会員登録してください。"
+      redirect_to new_end_user_registration_path
+    end
   end
 
 end
