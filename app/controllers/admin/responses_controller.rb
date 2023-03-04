949a0d0d40c7c0fd60fba7f04bb471ea2e9da0c7
Yui Tanabe <ytogt356@gmail.com>
2023-03-04 15:23:56 +0000

    [Add] 管理者側アンケート結果ページのレイアウト作成

--- app/controllers/admin/responses_controller.rb
+++ app/controllers/admin/responses_controller.rb
@@ -1,10 +1,10 @@
 class Admin::ResponsesController < ApplicationController
   def index
     @chart_responses = Response.group(:present_genre).order('count(id) desc')
-    @responses = Response.all
+    @responses = Response.order(created_at: "DESC").page(params[:page]).per(30)
     if params[:internet_protocol_address]
       @ip_address = params[:internet_protocol_address]
-      @same_end_user_responses = Response.where(internet_protocol_address: @ip_address)
+      @same_end_user_responses = Response.where(internet_protocol_address: @ip_address).order(created_at: "DESC").page(params[:page]).per(30)
     end
   end
   
